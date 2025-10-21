3b9e95677c78f01cfdfb6a4bc41fa2c70bd89c3b
Leandro wilker <leandrowilker99@gmail.com>
2025-10-21 00:57:50 -0300

    add bootstrap ao projeto
    novo layout

--- projeto-predmax/scripts/header.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.getElementById("menu-toggle");
-const menu = document.getElementById("menu");
-
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  menuToggle.classList.toggle("active");
-});
